7ec34858703c3a575b790f0408b48a4b20611317
gunax <socrates17@gmail.com>
2016-06-29 14:51:53 -0500

    done

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add(a,b)
+  a+b
+end
+
+def subtract(a,b)
+  a-b
+end
+
+def sum(a,c=0)
+  if a.length == 0
+    c
+  else
+    c+sum(a[1..-1],a[0])
+  end
+end
+
+def multiply(*c)
+  if c.length==0
+    0
+  else
+    multiply(c[0],c[1..-1])
+  end
+end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,12 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize(t=0)
+    @seconds=t
+  end
+  def time_string
+    a = "%02d" % (seconds/3600)
+    b = "%02d" % ((seconds%3600)/60)
+    c = "%02d" % (seconds%60)
+    [a,b,c].join(':')
+  end
 end
